1c10798f17ea961ddbba168ba93dc44d53b643d0
Valeria Oshiro <valeriaoshiro19@gmail.com>
2019-01-28 22:06:05 -0800

    Create warning component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MessagesComponent } from './messages/messages.component';
 import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessagesComponent,
-    SuccessComponent
+    SuccessComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule,
